cc19552cccab5606a3e041e141f82bdb2a1c6f36
Suhyen Im <suhyenim.kor@gmail.com>
2026-04-29 09:54:56 +0900

    chore: 분산 트레이싱(OpenTelemetry/Tempo) 제거 (#26)

--- src/main/java/com/opentraum/payment/PaymentServiceApplication.java
+++ src/main/java/com/opentraum/payment/PaymentServiceApplication.java
@@ -9,8 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
-        // Reactor Context <-> MDC/ThreadLocal 자동 전파 (Micrometer Tracing traceId MDC 포함)
-        reactor.core.publisher.Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 }
